2ae37e0f5839d4da83b5946559186662bb0bb4ae
VictorXie03 <victorxie75@gmail.com>
2024-02-17 06:15:30 -0500

    removed project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import capidexdemo from '../assets/capidexdemo.png'
 import wordledemo from '../assets/wordledemo.png'
 import elektroniksdemo from '../assets/elektroniksdemo.png'
-import ayodemo from '../assets/ayodemo.png'
 import taskoredemo from '../assets/taskoredemo.png'
 
 
@@ -58,21 +57,6 @@ const Projects = () => {
                             </span>
                             <div className='pt-8 text-center'>
                                 <a href="https://github.com/VictorXie03/E-lektroniks" target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ backgroundImage: `url(${ayodemo})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Asian Yummy One
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://github.com/VictorXie03/AYOWEB" target="_blank">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
